a231f019f8e642df56d70665decb34491f63ab13
likejean <popachs@yahoo.com>
2020-03-29 16:32:45 -0700

    Added Jest Dependencies and test files

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,4 @@
 import React from 'react';
 
-export default props => <button className="w3-btn w3-border w3-hover-blue" onClick={() => props.setSubmit(props.inputs)}>SUBMIT</button>
+const Button = ({ setSubmit, inputs }) => <button data-testid="button" className="w3-btn w3-border w3-hover-blue" onClick={() => setSubmit(inputs)}>SUBMIT</button>
+export default Button;
